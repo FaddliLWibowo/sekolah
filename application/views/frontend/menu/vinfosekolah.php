09e97ead6acdd9bad1ea1e6612229759ac91f3fa
ngadiono <cristiano_ondoel@yahoo.com>
2012-03-12 02:01:29 +0700

    betulin min height

--- application/views/frontend/menu/vinfosekolah.php
+++ application/views/frontend/menu/vinfosekolah.php
@@ -3,10 +3,7 @@
         <h2><i class="icon-leaf"></i>info sekolah</h2>
         <ul>
             <li>as</li>
-            <li>as</li>
-            <li>as</li>
-            <li>as</li>
-            <li>as</li>
+
         </ul>
     </div>
 
